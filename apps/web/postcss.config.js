8a643b3761133273733ad6018c7f258bd7368e02
Nuno Castro <nunomacastro@gmail.com>
2026-07-07 09:13:11 +0100

    Update: Reset

--- apps/web/postcss.config.js
+++
@@ -1,9 +0,0 @@
-/**
- * Configura a ferramenta associada a postcss.config.js para o ambiente real_dev da StudyFlow.
- */
-export default {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
-};
